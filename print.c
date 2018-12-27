3922623e3c79f3fda6d8a3a8844b0499b8b53711
Plutone11011 <lorenzo97@fastwebnet.it>
2018-12-27 21:12:09 +0100

    corretti errori per mipsel ma dà problemi convertitore elf

--- print.c
+++ print.c
@@ -2,22 +2,22 @@
 
 
 void print(char *str){
-    //trova l'indirizzo del registro del terminale 0
-    unsigned int term0Addr = DEV_ADDR_BASE + 512 ;
-    //punta ai campi TRANSM_STATUS e TRANSM_COMMAND del registro 
-    unsigned int *term0AddrStatus = (int*)(term0Addr + 0x8) ;
-    unsigned int *term0AddrCommand = (int*)(term0Addr + 0xC) ;
+    /*trova l'indirizzo del registro del terminale 0*/
+    volatile unsigned int term0Addr = DEV_ADDR_BASE + 512 ;
+    /*punta ai campi TRANSM_STATUS e TRANSM_COMMAND del registro*/ 
+    volatile unsigned int *term0AddrStatus = (volatile unsigned int*)(term0Addr + 0x8) ;
+    volatile unsigned int *term0AddrCommand = (volatile unsigned int*)(term0Addr + 0xC) ;
 
     unsigned int i, tmp ;
     for (i = 0; i < *(str + i); i++){
-        //seleziona solo lo status byte
+        /*seleziona solo lo status byte*/
         do{
             tmp = (*term0AddrStatus) & 0xFF ;
         } while ((tmp != READY_STAT) && (tmp != TRANSMITTED_STAT)) ;
-        //shifta l'i-esimo carattere di un byte a sinistra perché il carattere da trasmettere
-        //va nel secondo byte meno significativo mentre il comando va nel primo
+        /*shifta l'i-esimo carattere di un byte a sinistra perché il carattere da trasmettere
+        va nel secondo byte meno significativo mentre il comando va nel primo*/
         tmp = ((unsigned int)(*(str + i)) << 8) | TRANSMIT_CHAR ;
-        //copia tmp nel registro per lanciare comando
+        /*copia tmp nel registro per lanciare comando*/
         *term0AddrCommand = tmp ;
         
     }
