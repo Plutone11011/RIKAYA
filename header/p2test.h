cbd3cc590d1637d771332aa56df591365478fdbd
Plutone11011 <lorenzo97@fastwebnet.it>
2019-05-30 13:12:05 -0400

    p2test

---
+++ header/p2test.h
@@ -0,0 +1,28 @@
+#ifndef P2TEST_H
+#define P2TEST_H
+
+#include "scheduler.h"
+#include "handler.h"
+#include "interrupts.h"
+
+void print(char *msg);
+
+void test();
+void p2();
+void p3();
+void p4();
+void p5prog();
+void p5mm();
+void p5();
+void p5a();
+void p5b();
+void p6();
+void p7();
+void p8root();
+void child1();
+void intermediate();
+void child2();
+void p8leaf();
+void curiousleaf();
+
+#endif
